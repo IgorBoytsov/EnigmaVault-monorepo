064f96b3d5038becdb5f7dc1da750c421b9e6ef6
Light Play <lightplay456@gmail.com>
2025-07-16 11:31:35 +0300

    refactor(domain): внедрить класс Guard для проверок предусловий
    Добавлен статический служебный класс Guard, предназначенный для централизации и упрощения проверок входных данных.
    
    Он позволяет заменить повторяющиеся конструкции if-throw на лаконичные однострочные вызовы, что повышает читаемость и надежность доменной логики.

---
+++ apps/apis/EnigmaVault.SecretService.Api/EnigmaVault.SecretService.Domain/Guard.cs
@@ -0,0 +1,13 @@
+﻿namespace EnigmaVault.SecretService.Domain
+{
+    public static class Guard
+    {
+        public static void Against(bool condition, Func<Exception> exceptionFactory)
+        {
+            if (condition)
+            {
+                throw exceptionFactory();
+            }
+        }
+    }
+}
